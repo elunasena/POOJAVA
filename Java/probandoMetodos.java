1169901d2e8234536618b33e1fc6a746c104a024
AR.elunasen <lunasena.esteban@gmail.com>
2023-06-19 09:06:45 -0500

    19JUN23_0906

---
+++ Java/probandoMetodos.java
@@ -0,0 +1,17 @@
+
+public class probandoMetodos {
+
+	public static void main(String[] args) {
+
+		Cuenta c1 = new Cuenta();
+		Cuenta c2 = new Cuenta();
+		c1.saldo = 100;
+		c1.numero = 123;
+		c2.depositar(150);
+		System.out.println("Saldo C1: " + c1.saldo);
+		System.out.println("Saldo C2: " + c2.saldo);
+		c2.retirar(400);
+		System.out.println("Saldo C2: " + c2.saldo);
+
+	}
+}
