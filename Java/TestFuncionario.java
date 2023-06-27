5c5dad25a33560c86a9113675e92c901dc447ead
AR.elunasen <lunasena.esteban@gmail.com>
2023-06-27 17:28:30 -0500

    27JUN23_1728

---
+++ Java/TestFuncionario.java
@@ -0,0 +1,13 @@
+
+public class TestFuncionario {
+	
+	public static void main(String[] args) {
+		
+		Funcionario f1 = new Funcionario("Diego","1111",125.25);
+		
+		System.out.println(Funcionario.getFuncTotales());
+		
+		
+	}
+	
+}
